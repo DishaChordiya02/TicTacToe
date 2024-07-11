5ec8e710e3bfd9939cc0f6cdde1524dafad668af
DishaChordiya02 <143215163+DishaChordiya02@users.noreply.github.com>
2024-07-12 00:11:00 +0530

    Update TicTacToe.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -87,9 +87,17 @@ def switchplayer():
         currentplayer="O"
     else:
         currentplayer="X"
+
+def computer(board):
+    while currentPlayer == "O":
+        position = random.randint(0, 8)
+        if board[position] == "-":
+            board[position] = "O"
+            switchPlayer()
 while gamerunning:
     printboard(board)
     playerinput(board)
     checkwin()
     checktie(board)
     switchplayer()
+    computer(board)
